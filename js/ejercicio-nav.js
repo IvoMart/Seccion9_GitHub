1baf545a18821bd34b40527192d45f5a0286a4d4
IvoMart <ivo_ivancho@hotmail.com>
2023-05-31 01:17:02 -0300

    Compartir Practica de pagina web con secciones: header - main - footer

---
+++ js/ejercicio-nav.js
@@ -0,0 +1,9 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function () {
+        document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+    });
+
+
+const showAlert = ()=>alert("Clickeaste el botón.") ;
+
